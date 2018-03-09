7ce9c7e104cbe0738c66ec294cd6b16fc72fcbc8
danniela <dannie12@hotmail.es>
2018-03-08 21:36:56 -0600

    Ionic

---
+++ PruebaUno/src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { ChartModule } from 'angular2-highcharts';
+import * as highcharts from 'Highcharts';
+
+//import * as d3 from 'd3';
+
+//import * as functionplot from 'function-plot';
+
+//import * as mathjs from 'mathjs';
+
+
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp), ChartModule.forRoot(highcharts)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
